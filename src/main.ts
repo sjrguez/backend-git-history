091c0694daf5eb2cb2cec7976324c612e83ddac2
starlin de la cruz <starlinr82@gmail.com>
2023-10-14 22:34:14 -0400

    Add: Swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,14 @@ async function bootstrap() {
   })
 
   const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Github History')
+    .setDescription('The github history API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const port = configService.get<number>('PORT') || 3000;
 
